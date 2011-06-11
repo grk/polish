5de24b6c253eedabfbbd1fa6c9e631bf5d20b2a6
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2011-06-11 16:17:13 +0200

    Formatting

--- lib/polish.rb
+++ lib/polish.rb
@@ -1,5 +1,5 @@
-# -*- encoding: utf-8 -*- 
- 
+# -*- encoding: utf-8 -*-
+
 if RUBY_VERSION < "1.9"
   $KCODE = 'u'
 end
@@ -8,39 +8,39 @@ require 'i18n'
 
 # Rails hacks
 if defined?(ActionView::Helpers)
-  require 'polish/action_view_ext/helpers/date_helper' 
+  require 'polish/action_view_ext/helpers/date_helper'
 end
 
 require 'polish/proxies'
- 
+
 module Polish
   extend self
-  
+
   module VERSION
     STRING = File.open(File.dirname(__FILE__) + "/../VERSION").gets.chomp
     MAJOR, MINOR, TINY = STRING.split('.')
   end
-  
+
   # Polish locale
   LOCALE = :'pl'
- 
+
   # Polish locale
   def locale
     LOCALE
   end
-  
-  # Init Polish i18n: set custom backend, 
+
+  # Init Polish i18n: set custom backend,
   # load all translations shipped with library.
   def init_i18n
     I18n.backend.class.send(:include, I18n::Backend::Pluralization)
     I18n.load_path.unshift(*locale_files)
   end
-  
+
   protected
     # Returns all locale files shipped with library
     def locale_files
       Dir[File.join(File.dirname(__FILE__), "polish", "locale", "**/*")]
     end
 end
- 
+
 Polish.init_i18n
